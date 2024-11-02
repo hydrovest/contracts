65fa53eecbc8bd87f73bb63cb5aaf6b5e89e3060
Septian Maulana <septiandev028@gmail.com>
2024-11-02 17:31:02 +0700

    docs(readme): update readme
    - Add LICENSE
    - Add CONTRIBUTING
    - Add .env.example
    - Update variable to easy identify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { task, type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 import {
-  ETHERSCAN_API,
+  ARBISCAN_API,
   RPC_URL,
   USER_PRIVATE_KEY,
 } from "./scripts/helpers/constants";
@@ -16,15 +16,11 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
     apiKey: {
-      arbitrumSepolia: ETHERSCAN_API,
+      arbitrumSepolia: ARBISCAN_API,
     },
   },
   sourcify: {
-    // Disabled by default
-    // Doesn't need an API key
     enabled: true,
   },
 };
